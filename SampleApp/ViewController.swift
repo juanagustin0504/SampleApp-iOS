5eaa1c89ee6ab9811beb14b4e736308850d3c520
juanagustin0504 <juanagustin@naver.com>
2019-11-18 15:14:13 +0900

    add location permission

--- SampleApp/ViewController.swift
+++ SampleApp/ViewController.swift
@@ -7,13 +7,49 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    
+    var locLast : CLLocationCoordinate2D?
+    var getLocation = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
     }
+    
+    @IBAction func btnGetLocalLocation(_ sender: UIButton) {
+        
+        locationManager.requestWhenInUseAuthorization()
+        
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+            
+            if getLocation {
+                let alert = UIAlertController(title: "위치", message: "위도 : \((locLast?.latitude)!)\n경도 : \((locLast?.longitude)!)", preferredStyle: .alert)
+                let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alert.addAction(alertAction)
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue : CLLocationCoordinate2D = manager.location!.coordinate
+        
+        locLast = locValue
+        
+        let alert = UIAlertController(title: "위치", message: "위도 : \(locValue.latitude)\n경도 : \(locValue.longitude)", preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(alertAction)
+        self.present(alert, animated: true, completion: nil)
+        getLocation = true
+    }
+    
     
 }
