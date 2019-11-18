af7bee2e8fa6835ef5ec5f1e1b9843b25894c37f
juanagustin0504 <juanagustin@naver.com>
2019-11-18 13:03:44 +0900

    add button(비즈플레이로 이동), add WebView

---
+++ WebViewController.swift
@@ -0,0 +1,90 @@
+//
+//  WebViewController.swift
+//  SampleApp
+//
+//  Created by Webcash on 2019/11/18.
+//  Copyright © 2019 Moon. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
+
+    var webView: WKWebView!
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+    
+    override func loadView() {
+        super.loadView()
+        webView = WKWebView(frame: self.view.frame)
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+        
+        self.view = self.webView!
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        let lnk = "https://www.bizplay.co.kr/"
+        let url = URL(string: lnk)
+        let request = URLRequest(url: url!)
+        
+        
+        webView.load(request)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @available(iOS 8.0, *)
+    public func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Swift.Void) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let otherAction = UIAlertAction(title: "OK", style: .default, handler: {action in completionHandler()})
+        alert.addAction(otherAction)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @available(iOS 8.0, *)
+    public func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Swift.Void) {
+        let alert = UIAlertController(title: "", message: message, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "CANCEL", style: .cancel, handler: {(action) in completionHandler(false)})
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: {(action) in completionHandler(true)})
+        alert.addAction(cancelAction)
+        alert.addAction(okAction)
+        
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+    @available(iOS 8.0, *)
+    public func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        activityIndicator = UIActivityIndicatorView(style: .whiteLarge)
+        activityIndicator.frame = CGRect(x: view.frame.midX - 50, y: view.frame.midY - 50, width: 100, height: 100)
+        activityIndicator.color = UIColor.gray
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.startAnimating()
+        self.view.addSubview(activityIndicator)
+    }
+    
+    @available(iOS 8.0, *)
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        self.activityIndicator.removeFromSuperview()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
