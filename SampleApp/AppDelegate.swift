8384b642c6257168667c9f13e2c5685c8e4276ff
juanagustin0504 <juanagustin@naver.com>
2019-11-25 16:40:16 +0900

    Added db_data.json, DbVersionData.swift

--- SampleApp/AppDelegate.swift
+++ SampleApp/AppDelegate.swift
@@ -14,7 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
         FirebaseApp.configure()
         return true
     }
@@ -32,31 +31,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-    
-    // 홈 버튼을 눌렀을 때(Home Button Click) //
-    func applicationWillResignActive(_ application: UIApplication) {
-        print("applicationWillResignActive")
-    }
-    
-    // 홈 버튼을 눌러서 백그라운드로 전환되었을 때(Enter Background) //
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        print("applicationDidEnterBackground")
-    }
-    
-    // 백그라운드에서 포그라운드로 넘어올 때(Background -> Foreground) //
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        print("applicationWillEnterForeground")
-    }
-    
-    // 액티브로 전환되었을 때(Active) //
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        print("applicationDidBecomeActive")
-    }
-    
-    // 앱이 종료될 때(Terminate App) //
-    func applicationWillTerminate(_ application: UIApplication) {
-        print("applicationWillTerminate")
-    }				
+    				
 
 }
 
--- SampleApp/NetworkingViewController.swift
+++ SampleApp/NetworkingViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
-import FirebaseDatabase
+import Firebase
 
 class NetworkingViewController: UIViewController {
     
     var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+    
+    var ref : DatabaseReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,61 +30,81 @@ class NetworkingViewController: UIViewController {
         
         // 서버 통신 시작 //
         
-        let ref = Database.database().reference()
         
-        ref.child("init/init_data/").observeSingleEvent(of: .value) {
-            (snapshot) in
-            let initData = snapshot.value as? [String : String]
+        ref = Database.database().reference()
+        
+        ref.child("version").observeSingleEvent(of: .value, with: { snapShot in
+            let versionDic = snapShot.value as? Dictionary<String, AnyObject>
             
-            self.activityIndicator.removeFromSuperview()
-            
-//            print(initData!)
-            
-            if let maintainance = initData!["maintainance"] {
-                if maintainance.elementsEqual("true") {
-                    // 서비스 점검 중일 때 //
-                    let alert = UIAlertController(title: nil, message: "서비스 점검 중입니다.", preferredStyle: .alert)
-                    let action = UIAlertAction(title: "확인", style: .cancel, handler: {
-                        (action) in exit(0)
-                    })
-                    
-                    alert.addAction(action)
-                    self.present(alert, animated: true)
-                }
-            }
-            
-            if let requiredUpdate = initData!["required_update"] {
-                if requiredUpdate.elementsEqual("true") {
-                    // 앱 업데이트 후 이용해야 할 때 //
-                    let alert = UIAlertController(title: nil, message: "앱 업데이트 후 이용해주세요.", preferredStyle: .alert)
-                    let action = UIAlertAction(title: "확인", style: .cancel, handler: {
-                        (action) in exit(0)
-                    })
-                    
-                    alert.addAction(action)
-                    self.present(alert, animated: true)
-                }
-            }
-            
-            if let selectUpdate = initData!["select_update"] {
-                if selectUpdate.elementsEqual("true") {
-                    // 앱 업데이트 선택할 때 //
-                    let alert = UIAlertController(title: nil, message: "앱 업데이트 후 이용하시겠습니까?", preferredStyle: .alert)
-                    let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
-                    let okAction = UIAlertAction(title: "확인", style: .cancel, handler: {
-                        (action) in exit(0)
-                    })
-                    
-                    alert.addAction(cancelAction)
-                    alert.addAction(okAction)
-                    self.present(alert, animated: true)
-                }
-            }
+            let versionDbData = DbVersionData()
+            versionDbData.setValuesForKeys(versionDic!)
+           
+            print("[force_update_message] : \(versionDbData.force_update_message)")
+            print("[lastest_version_code] : \(versionDbData.lastest_version_code)")
+            print("[minimum_version_name] : \(versionDbData.minimum_version_name)")
+            print("[optional_update_message] : \(versionDbData.optional_update_message)")
+        
+        })
+        
+        self.activityIndicator.removeFromSuperview()
+        
+        
+    } // end of viewDidLoad
+    
+    // 버전 체크 //
+    func checkUpdateVersion(dbdata:DbVersionData){
+        let appLastestVersion = dbdata.lastest_version_code as String
+        let appMinimumVersion = dbdata.minimum_version_code as String
+        
+        let infoDic = Bundle.main.infoDictionary!
+        let appBuildVersion = infoDic["CFBundleVersion"] as? String
+        
+        if (Int(appBuildVersion!)! < Int(appMinimumVersion)!) {
+            // 강제업데이트 //
+            forceUdpateAlert(message: dbdata.force_update_message)
+        }else if(Int(appBuildVersion!)! < Int(appLastestVersion)!) {
+            // 선택업데이트 //
+            optionalUpdateAlert(message: dbdata.optional_update_message, version: Int(dbdata.lastest_version_code)!)
         }
+     } // end of checkUpdateVersion
+    
+    // 강제업데이트 //
+    func forceUdpateAlert(message:String) {
         
+        let refreshAlert = UIAlertController(title: "UPDATE", message: message, preferredStyle: UIAlertController.Style.alert)
+        
+        refreshAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action: UIAlertAction!) in
+            print("Go to AppStore")
+            // AppStore 로 가도록 연결시켜 주면 됩니다.
+        }))
+        
+        self.present(refreshAlert, animated: true, completion: nil)
+
+    } // end of forceUpdateAlert
+    
+    
+    // 선택업데이트 //
+    func optionalUpdateAlert(message:String, version:Int) {
+        
+        let refreshAlert = UIAlertController(title: "UPDATE", message: message, preferredStyle: UIAlertController.Style.alert)
+        
+        refreshAlert.addAction(UIAlertAction(title: "Update", style: .default, handler: { (action: UIAlertAction!) in
+            print("Go to AppStore")
+        }))
+        
+        refreshAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action: UIAlertAction!) in
+            print("Close Alert")
+        }))
+        
+        self.present(refreshAlert, animated: true, completion: nil)
+        
+    } // end of optionalUpdateAlert
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
-
     /*
     // MARK: - Navigation
 
